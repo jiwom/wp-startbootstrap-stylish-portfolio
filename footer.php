52c30abb126319733ebb137bfd65ce4a27cd55d0
Jerome Sarmiento <jsarmiento@ltcglobal.com>
2016-09-22 15:45:39 +0800

    Add CustomField and change Text

--- footer.php
+++ footer.php
@@ -3,29 +3,29 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-10 col-lg-offset-1 text-center">
-                <h4><strong>Start Bootstrap</strong>
+                <h4><strong>Contact Me</strong>
                 </h4>
-                <p>3481 Melrose Place
-                    <br>Beverly Hills, CA 90210</p>
+                <p><?php echo get_post_meta($post->ID, 'footer-address', true); ?></p>
                 <ul class="list-unstyled">
-                    <li><i class="fa fa-phone fa-fw"></i> (123) 456-7890</li>
-                    <li><i class="fa fa-envelope-o fa-fw"></i> <a href="mailto:name@example.com">name@example.com</a>
+                    <li><i class="fa fa-phone fa-fw"></i> <?php echo get_post_meta($post->ID, 'footer-phone', true); ?></li>
+                    <li><i class="fa fa-envelope-o fa-fw"></i> <a href="mailto:<?php echo get_post_meta($post->ID, 'footer-email', true); ?>">
+                            <?php echo get_post_meta($post->ID, 'footer-email', true); ?></a>
                     </li>
                 </ul>
                 <br>
                 <ul class="list-inline">
                     <li>
-                        <a href="#"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
+                        <a href="<?php echo get_post_meta($post->ID, 'footer-fb', true); ?>" target="_blank"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
+                        <a href="<?php echo get_post_meta($post->ID, 'footer-linkedin', true); ?>" target="_blank"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-dribbble fa-fw fa-3x"></i></a>
+                        <a href="<?php echo get_post_meta($post->ID, 'footer-github', true); ?>" target="_blank"><i class="fa fa-github fa-fw fa-3x"></i></a>
                     </li>
                 </ul>
                 <hr class="small">
-                <p class="text-muted">Copyright &copy; Your Website 2016</p>
+                <p class="text-muted">Copyright &copy; Jiwom 2016</p>
             </div>
         </div>
     </div>
